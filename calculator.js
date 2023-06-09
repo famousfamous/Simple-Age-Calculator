b26a9f1c474cf39e1456b4cf176c27c2c1e00901
famousfamous <writefamous@gmail.com>
2023-06-09 16:26:44 +0100

    My age calculator app

--- calculator.js
+++ calculator.js
@@ -1,15 +1,33 @@
-function checkDate() {
-  let userDate = document.getElementById("dateBirthdate").value;
-  let formattedDate = new Date();
-
-  let oldDate = new Date(userDate).getDate();
-  let newDate = formattedDate.getDate();
-  if (oldDate > newDate) {
-    console.log(oldDate - newDate);
-  }
+//code to add today
+function setDefaultDate() {
+    var date = new Date();
+    var day = date.getDate();
+    var month = date.getMonth() + 1;
+    var year = date.getFullYear();
+    if (month < 10) month = "0" + month;
+    if (day < 10) day = "0" + day;
+    
+    var today = year + "-" + month + "-" + day; 
+    document.getElementById("dateBirthdate").value = today;
+    document.getElementById("dateBirthdate").setAttribute('max', today);
+    console.log(today);
 }
 
+//code to determine if user is choosing a later date than today
+let dateExtract = document.getElementById("alive");
+
+dateExtract.addEventListener('change', calculate);
+
 function calculate() {
+  let isUserAlive = document.querySelector('input[type="checkbox"]');
+  if (isUserAlive.checked) {
+    document.getElementById("shortInfo").innerHTML = "You have been enjoying life for:";
+    document.getElementById('shortInfo').style.color = 'green';
+  } else if (!isUserAlive.checked) {
+    document.getElementById("shortInfo").innerHTML = "The Person lived for:";
+    document.getElementById('shortInfo').style.color = 'yellow';
+  }
+
   setInterval(() => {
     let birthdate = new Date(document.getElementById("dateBirthdate").value);
 
@@ -25,6 +43,7 @@ function calculate() {
     let ageInYears = ageInMonths / 12; // to years
 
     //now we begin to populate the DOM with the compute
+
     document.querySelector("#years").innerHTML = Math.floor(ageInYears);
     document.querySelector("#months").innerHTML = Math.floor(ageInMonths % 12);
     document.querySelector("#days").innerHTML = Math.floor(ageInDays % 30.4375);
